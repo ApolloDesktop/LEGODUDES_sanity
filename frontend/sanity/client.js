9152bdaba3841820e9be7f03fa8faaafe9666847
ApolloDesktop <apollogamestuff@gmail.com>
2024-04-16 13:52:39 +0200

    Init reviews & writeClient basics + fix av CSS feil

--- frontend/sanity/client.js
+++ frontend/sanity/client.js
@@ -6,3 +6,14 @@ export const client = createClient({
     useCdn: true,
     apiVersion: "2022-03-07"
 })
+
+/* skXaSWaibQWlJnodkHSV0X85V6d6lnOr7GGzn6UvEVfLSfs553l4WWVTsgHsqRawPIQNtxPEr6yXXASTG1FOKFT
+pBq7g1MbhDVVlpKPBDfVXtup7QDuLcqt0G6qb1klgBSaWKExC1H5zZl3y3dB1379Smvs6SbGdLfU1RjhQhT571bwmp17n */
+
+export const writeClient = createClient({
+    projectId: "v74qoqsz",
+    dataset: "production",
+    useCdn: false,
+    apiVersion: "2022-03-07",
+    token: "skXaSWaibQWlJnodkHSV0X85V6d6lnOr7GGzn6UvEVfLSfs553l4WWVTsgHsqRawPIQNtxPEr6yXXASTG1FOKFTpBq7g1MbhDVVlpKPBDfVXtup7QDuLcqt0G6qb1klgBSaWKExC1H5zZl3y3dB1379Smvs6SbGdLfU1RjhQhT571bwmp17n"
+})
